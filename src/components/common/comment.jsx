823cf86accea93b78fa82d35566d0d40059ac086
satwik1608 <satwikkashyap1608@gmail.com>
2023-01-31 16:20:34 +0530

    Multiple reply support

--- src/components/common/comment.jsx
+++ src/components/common/comment.jsx
@@ -3,17 +3,14 @@ import CommentForm from "./commentForm";
 import UserContext from "./../../userContext";
 const { formatDate } = require("../../services/utils");
 function Comment({ comment, onChange }) {
-  const [reply, setReply] = React.useState(false);
   const [replyOpen, setReplyOpen] = React.useState(false);
   const { id: user } = React.useContext(UserContext);
-  React.useEffect(() => {
-    if (!comment.reply) setReply(false);
-    else setReply(true);
-  }, []);
+  const [viewReply, setviewReply] = React.useState(false);
+  React.useEffect(() => {}, [replyOpen]);
 
   const onReset = () => {
-    setReply(true);
     setReplyOpen(false);
+    setviewReply(true);
   };
 
   return (
@@ -39,31 +36,54 @@ function Comment({ comment, onChange }) {
       <div class="text-gray-500 dark:text-gray-400 break-words">
         {comment.data}
       </div>
-      <div class="flex items-center mt-4 space-x-4 	">
-        {!reply && user && (
-          <button
-            type="button"
-            class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-            onClick={() => setReplyOpen((r) => !r)}
-          >
-            <svg
-              aria-hidden="true"
-              class="mr-1 w-4 h-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+      <div class="items-center mt-4 space-x-4 	">
+        <div className="flex space-x-4">
+          {user && (
+            <div>
+              <button
+                type="button"
+                class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
+                onClick={() => setReplyOpen((r) => !r)}
+              >
+                <svg
+                  aria-hidden="true"
+                  class="mr-1 w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                  ></path>
+                </svg>
+                Reply
+              </button>
+            </div>
+          )}
+          {viewReply && (
+            <button
+              type="button"
+              onClick={() => setviewReply((c) => !c)}
+              class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-              ></path>
-            </svg>
-            Reply
-          </button>
-        )}
+              Hide Replies
+            </button>
+          )}
+          {!viewReply && (
+            <button
+              type="button"
+              onClick={() => setviewReply((c) => !c)}
+              class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
+            >
+              View Replies
+            </button>
+          )}
+        </div>
+
         {replyOpen && user && (
           <CommentForm
             blog={comment.blog}
@@ -73,31 +93,33 @@ function Comment({ comment, onChange }) {
             onReset={onReset}
           />
         )}
-        {comment.reply && (
-          <div
-            class="p-6 mb-6 text-base bg-#1f2937 rounded-lg max-w-full dark:bg-#1f2937"
-            key={comment.reply.author._id}
-          >
-            <footer class="flex justify-between items-center mb-2">
-              <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm  text-gray-900 dark:text-white">
-                  <img
-                    class="mr-2 w-6 h-6 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                    alt="Michael Gough"
-                  />
-                  {comment.reply.author.name}
-                </p>
-                <p class="text-sm text-gray-600 dark:text-gray-400">
-                  <div>{formatDate(comment.reply.date)}</div>
-                </p>
+        {viewReply &&
+          comment.reply &&
+          comment.reply.map((cmtRep) => (
+            <div
+              class="p-3 mt-1 text-base bg-#1f2937 rounded-lg max-w-full dark:bg-#1f2937"
+              key={cmtRep.author._id}
+            >
+              <footer class="flex justify-between items-center mb-2">
+                <div class="flex items-center">
+                  <p class="inline-flex items-center mr-3 text-sm  text-gray-900 dark:text-white">
+                    <img
+                      class="mr-2 w-6 h-6 rounded-full"
+                      src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                      alt="Michael Gough"
+                    />
+                    {cmtRep.author.name}
+                  </p>
+                  <p class="text-sm text-gray-600 dark:text-gray-400">
+                    <div>{formatDate(cmtRep.date)}</div>
+                  </p>
+                </div>
+              </footer>
+              <div class="text-gray-500 dark:text-gray-400 break-words ">
+                {cmtRep.data}
               </div>
-            </footer>
-            <div class="text-gray-500 dark:text-gray-400 break-words ">
-              {comment.reply.data}
             </div>
-          </div>
-        )}
+          ))}
       </div>
     </div>
   );
--- src/components/common/commentForm.jsx
+++ src/components/common/commentForm.jsx
@@ -27,9 +27,8 @@ function CommentForm({ blog, onChange, isReply, id, onReset }) {
       comment = {
         reply: com.data._id,
       };
-
-      await updateComment(comment, id);
       onReset();
+      await updateComment(comment, id);
     } else {
       const comment = {
         data: data,
@@ -53,22 +52,45 @@ function CommentForm({ blog, onChange, isReply, id, onReset }) {
     <form className="mb-6" onSubmit={handleSubmit}>
       <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg  border border-gray-100 border-round dark:bg-gray-800 dark:border-gray-700">
         <label for="comment" className="sr-only"></label>
-        <textarea
-          id="comment"
-          rows="6"
-          className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-          placeholder="Write a comment..."
-          ref={textRef}
-          onChange={handleChange}
-          required
-        />
+        {isReply && (
+          <textarea
+            id="comment"
+            rows="6"
+            className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+            placeholder="Write a reply..."
+            ref={textRef}
+            onChange={handleChange}
+            required
+          />
+        )}
+        {!isReply && (
+          <textarea
+            id="comment"
+            rows="6"
+            className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+            placeholder="Write a comment..."
+            ref={textRef}
+            onChange={handleChange}
+            required
+          />
+        )}
       </div>
-      <button
-        type="submit"
-        className="inline-flex items-center btn-primary py-2.5 px-4 text-xs font-medium text-center  bg-blue-700 text-white rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-      >
-        Post comment
-      </button>
+      {!isReply && (
+        <button
+          type="submit"
+          className="inline-flex items-center btn-primary py-2.5 px-4 text-xs font-medium text-center  bg-blue-700 text-white rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+        >
+          Post comment
+        </button>
+      )}
+      {isReply && (
+        <button
+          type="submit"
+          className="inline-flex items-center btn-primary py-2.5 px-4 text-xs font-medium text-center  bg-blue-700 text-white rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+        >
+          Post Reply
+        </button>
+      )}
     </form>
   );
 }
