490eea382985e0a2d21c819da76c848ddb6c7f4b
satwik1608 <satwikkashyap1608@gmail.com>
2023-02-07 00:27:04 +0530

    remove tag follow

--- src/components/tagProfile.jsx
+++ src/components/tagProfile.jsx
@@ -10,12 +10,6 @@ function TagProfile() {
         <div class="mb-7  font-extrabold border-b border-gray-200 tracking-tight leading-none text-gray-900 md:text-xl lg:text-4xl dark:text-white ">
           {tag}
         </div>
-        <button
-          type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Follow
-        </button>
 
         <BlogList tag={tag} />
       </div>
