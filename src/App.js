31d00992f1cda3cba1f43c9a22d34149035a0faa
satwik1608 <satwikkashyap1608@gmail.com>
2023-02-07 23:33:02 +0530

    f

--- src/App.js
+++ src/App.js
@@ -31,50 +31,50 @@ function App() {
   const [id, setId] = React.useState();
 
   React.useEffect(() => {
-    var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-    var themeToggleLightIcon = document.getElementById(
-      "theme-toggle-light-icon"
-    );
+    // var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+    // var themeToggleLightIcon = document.getElementById(
+    //   "theme-toggle-light-icon"
+    // );
 
-    if (
-      localStorage.getItem("color-theme") === "dark" ||
-      (!("color-theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      themeToggleLightIcon.classList.remove("hidden");
-    } else {
-      themeToggleDarkIcon.classList.remove("hidden");
-    }
+    // if (
+    //   localStorage.getItem("color-theme") === "dark" ||
+    //   (!("color-theme" in localStorage) &&
+    //     window.matchMedia("(prefers-color-scheme: dark)").matches)
+    // ) {
+    //   themeToggleLightIcon.classList.remove("hidden");
+    // } else {
+    //   themeToggleDarkIcon.classList.remove("hidden");
+    // }
 
-    var themeToggleBtn = document.getElementById("theme-toggle");
+    // var themeToggleBtn = document.getElementById("theme-toggle");
 
-    themeToggleBtn.addEventListener("click", function () {
-      // toggle icons inside button
-      themeToggleDarkIcon.classList.toggle("hidden");
-      themeToggleLightIcon.classList.toggle("hidden");
+    // themeToggleBtn.addEventListener("click", function () {
+    //   // toggle icons inside button
+    //   themeToggleDarkIcon.classList.toggle("hidden");
+    //   themeToggleLightIcon.classList.toggle("hidden");
 
-      // if set via local storage previously
-      if (localStorage.getItem("color-theme")) {
-        // console.log(localStorage.getItem("color-theme"), "lol");
-        if (localStorage.getItem("color-theme") === "light") {
-          document.documentElement.classList.add("dark");
-          localStorage.setItem("color-theme", "dark");
-        } else {
-          document.documentElement.classList.remove("dark");
-          localStorage.setItem("color-theme", "light");
-        }
+    //   // if set via local storage previously
+    //   if (localStorage.getItem("color-theme")) {
+    //     // console.log(localStorage.getItem("color-theme"), "lol");
+    //     if (localStorage.getItem("color-theme") === "light") {
+    //       document.documentElement.classList.add("dark");
+    //       localStorage.setItem("color-theme", "dark");
+    //     } else {
+    //       document.documentElement.classList.remove("dark");
+    //       localStorage.setItem("color-theme", "light");
+    //     }
 
-        // if NOT set via local storage previously
-      } else {
-        if (document.documentElement.classList.contains("dark")) {
-          document.documentElement.classList.remove("dark");
-          localStorage.setItem("color-theme", "light");
-        } else {
-          document.documentElement.classList.add("dark");
-          localStorage.setItem("color-theme", "dark");
-        }
-      }
-    });
+    //     // if NOT set via local storage previously
+    //   } else {
+    //     if (document.documentElement.classList.contains("dark")) {
+    //       document.documentElement.classList.remove("dark");
+    //       localStorage.setItem("color-theme", "light");
+    //     } else {
+    //       document.documentElement.classList.add("dark");
+    //       localStorage.setItem("color-theme", "dark");
+    //     }
+    //   }
+    // });
     const author = getCurrentUser();
 
     if (author) {
