bde6e6672af7f758f0204ffbe50b83dd78174d2e
satwik1608 <satwikkashyap1608@gmail.com>
2023-02-12 15:01:05 +0530

    fixed bug

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -285,7 +285,7 @@ function RegisterForm() {
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full  bg-primary-600 dark:text-slate-100  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
               </button>
